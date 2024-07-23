0c2b6c193a4dce0df4f768659d27ae783fa0e105
Prabath Rathnamalala <prabathrathnamalala2k2@gmail.com>
2024-07-23 08:42:44 +0530

    sentry config

--- next.config.mjs
+++ next.config.mjs
@@ -13,7 +13,7 @@ export default withSentryConfig(withSentryConfig(nextConfig, {
 // For all available options, see:
 // https://github.com/getsentry/sentry-webpack-plugin#options
 
-org: "javascript-mastery",
+org: "pr-inovations",
 project: "care-pulse",
 
 // Only print logs for uploading source maps in CI
