c228987550e75bcfbb2165a5e4fc00849c8f0b66
Prabath Rathnamalala <prabathrathnamalala2k2@gmail.com>
2024-07-23 08:47:22 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -14,7 +14,7 @@ export default withSentryConfig(withSentryConfig(nextConfig, {
 // https://github.com/getsentry/sentry-webpack-plugin#options
 
 org: "pr-inovations",
-project: "care-pulse",
+project: "javascript-nextjs",
 
 // Only print logs for uploading source maps in CI
 silent: !process.env.CI,
